6607c764c240dd68bb0f3480ba769137de4d4e67
Kristoffer Walker <kris@kixx.name>
2014-03-23 23:08:19 -0400

    wordpress 3.8.1 files
     On branch wordpress_3_8_1

--- wp-content/themes/lazycrazyacres/sidebar.php
+++
@@ -1,9 +0,0 @@
-<ul class="sidebar <?php echo get_post_type(); ?>">
-<?php
-if (get_post_type() == 'page') {
-    dynamic_sidebar( 'main-sidebar' );
-} else {
-    dynamic_sidebar( 'blog-sidebar' );
-}
-?>
-</ul>
