df882f5280367bf4ce7ff6885e6dc9dc109b1786
Kristoffer Walker <kris@kixx.name>
2014-03-24 01:30:58 -0400

    configured and running locally
     On branch wordpress_3_8_1
    	renamed:    wp-config-sample.php -> wp-config.php

--- wp-config.php
+++ wp-config.php
@@ -16,13 +16,13 @@
 
 // ** MySQL settings - You can get this info from your web host ** //
 /** The name of the database for WordPress */
-define('DB_NAME', 'database_name_here');
+define('DB_NAME', 'lazycrazyacres');
 
 /** MySQL database username */
-define('DB_USER', 'username_here');
+define('DB_USER', 'wordpress');
 
 /** MySQL database password */
-define('DB_PASSWORD', 'password_here');
+define('DB_PASSWORD', 'wp_firesale');
 
 /** MySQL hostname */
 define('DB_HOST', 'localhost');
@@ -42,14 +42,14 @@ define('DB_COLLATE', '');
  *
  * @since 2.6.0
  */
-define('AUTH_KEY',         'put your unique phrase here');
-define('SECURE_AUTH_KEY',  'put your unique phrase here');
-define('LOGGED_IN_KEY',    'put your unique phrase here');
-define('NONCE_KEY',        'put your unique phrase here');
-define('AUTH_SALT',        'put your unique phrase here');
-define('SECURE_AUTH_SALT', 'put your unique phrase here');
-define('LOGGED_IN_SALT',   'put your unique phrase here');
-define('NONCE_SALT',       'put your unique phrase here');
+define('AUTH_KEY',         '`Gv S+YL+>_ki.#R^K{--/Tj4p8w uM?*91poxQDHc!poFsCWZ+ME-Jzf83(4uU{');
+define('SECURE_AUTH_KEY',  '~._&@Jc535!wT_x7`LB$R4E69!h4l]72_Tu/i4_#mzWW(og_R9;><Q;+^X],]Y!U');
+define('LOGGED_IN_KEY',    'QlfC+>r>Y`Xa@}mx`1m-jwbIDGlgiA;-q`2@iV[:+T[Kz,^|2~,~C8Yl[8/^tpU,');
+define('NONCE_KEY',        '}|+~WZ-j4RKgJ12=$BWc_JVl[B%imdS8xfyDff1C&#)%KQSIAX]t~FR=6=iTs8z{');
+define('AUTH_SALT',        ';;6RilfF&42Nq86=*}LMW $ogsx?2QU+{1bxFd>igO%6_w`6~x]7rUeA%z~V2+r;');
+define('SECURE_AUTH_SALT', 'kQwQrch _!-E-FZM$M[3$k15C9`csUev?W12rF: nX1OzCF_c+!W%Ebp{n7$[Wzg');
+define('LOGGED_IN_SALT',   '_af3Im.$=Hk--<qwE!-%[LO3Gz fO!;+Qo>: s`~Z6ds(lEgI12|IoZl? p_!LA;');
+define('NONCE_SALT',       'j$1M{B:uxNn3Q84E)hP; Gf+bDjDjGL8C&Eea^!4i`mUOx0?<<!i(*$c?->`J^o:');
 
 /**#@-*/
 
